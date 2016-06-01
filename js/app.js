a05aaa52520f39a4b5f7b4ebc2a9685b8fa96754
sawheeldon <simon@jampot.design>
2016-06-01 21:04:41 +0100

    Foundations

---
+++ js/app.js
@@ -0,0 +1,44 @@
+/* Global Variables */
+
+var questionsArray = [
+
+    //question 1
+    {
+    questionAsked:"Who is Manchester City's new manager for 2016/17 season.",
+    questionChoices:["Alex Ferguson", "Guardiola", "Mourinho", "Ancelotti"],
+    questionAnswer:1,
+    },
+
+    //question 2
+    {
+        questionAsked:"Who is Manchester City's new manager for 2016/17 season.",
+        questionChoices:["Alex Ferguson", "Guardiola", "Mourinho", "Ancelotti"],
+        questionAnswer:1,
+    },
+
+    //question 3
+    {
+        questionAsked:"Who is Manchester City's new manager for 2016/17 season.",
+        questionChoices:["Alex Ferguson", "Guardiola", "Mourinho", "Ancelotti"],
+        questionAnswer:1,
+    },
+
+
+
+]
+
+
+/* Function-definitions */
+
+
+
+
+/* Function-usage */
+
+$(document).ready(function () {
+
+
+
+
+
+});
