656b702cac4d3972eddb84951cbda49c6397d0bf
Simon Wheeldon <simon@jampot.design>
2016-06-02 12:05:09 +0100

    Building questions

--- js/app.js
+++ js/app.js
@@ -3,29 +3,71 @@
 var questionsArray = [
 
     //question 1
-    {
-    questionAsked:"Who is Manchester City's new manager for 2016/17 season.",
-    questionChoices:["Alex Ferguson", "Guardiola", "Mourinho", "Ancelotti"],
-    questionAnswer:1,
+        {
+            questionAsked: "Who is Manchester City's new manager for 2016/17 season.",
+            questionChoices: ["Alex Ferguson", "Guardiola", "Mourinho", "Ancelotti"],
+            questionAnswer: 1,
     },
 
     //question 2
-    {
-        questionAsked:"Who is Manchester City's new manager for 2016/17 season.",
-        questionChoices:["Alex Ferguson", "Guardiola", "Mourinho", "Ancelotti"],
-        questionAnswer:1,
-    },
+        {
+            questionAsked: "Who won the 2014/15 Premier League Season.",
+            questionChoices: ["Leicester City", "Arsenal", "Manchester City", "Chelsea".
+                "],
+        questionAnswer: 3,
+            },
 
-    //question 3
-    {
-        questionAsked:"Who is Manchester City's new manager for 2016/17 season.",
-        questionChoices:["Alex Ferguson", "Guardiola", "Mourinho", "Ancelotti"],
-        questionAnswer:1,
-    },
+            //question 3
+            {
+                questionAsked: "Where do Aston Villa play their home games.",
+                questionChoices: ["Villa Road", "Villa Park", "Sun Life Stadium", ],
+                questionAnswer: 1,
+            },
+
+            //question 4
+            {
+                questionAsked: "Who won the 2015/16 Champions Leage Final.",
+                questionChoices: ["Real Madrid", "Barcelona", "Argentina", "Bayern Munich"],
+                questionAnswer: 0,
+            },
+
+            //question 5
+            {
+                questionAsked: "Who play their homes games at St James' Park",
+                questionChoices: ["Goole Town", "Hartlepool", "Blackburn Rovers", "Exeter Town"],
+                questionAnswer: 3,
+            },
+
+            //question 6
+            {
+                questionAsked: "How many teams have won the Premier League Trophy.",
+                questionChoices: ["2", "5", "6", "8"],
+                questionAnswer: 2,
+            },
+
+            //question 7
+            {
+                questionAsked: "Who is the only player to have won silverware at both Man Utd and Liverpool.",
+                questionChoices: ["Owen", "Ince", "Hughes", "Beardsley"],
+                questionAnswer: 0,
+            },
+
+            //question 8
+            {
+                questionAsked: ".",
+                questionChoices: ["."],
+                questionAnswer: 1,
+            },
+
+            //question 9
+            {
+                questionAsked: ".",
+                questionChoices: ["."],
+                questionAnswer: 1,
+            },
 
 
-
-]
+            ]
 
 
 /* Function-definitions */
@@ -41,4 +83,4 @@ $(document).ready(function () {
 
 
 
-});
+        });
